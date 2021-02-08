94a7f0ec347c374dbbf0250d02e7de2f19279e62
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-02-08 22:49:39 +0200

    setting up entities

---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  NumberTrivia({
+    @required this.number,
+    @required this.text,
+  }) : super([text, number]);
+}
