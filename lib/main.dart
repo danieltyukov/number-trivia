9d23ed029a7b6fd97214e6e0146bbd3ddceb6bab
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-02-16 16:59:16 +0200

    bloc testing params

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,23 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'features/number_trivia/presentation/pages/number_trivia_page.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({this.title});
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void incrementValue() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  List<Color> _colorList = [Colors.red, Colors.blue];
-
-  int numType(_counter) {
-    return _counter % 2 == 0 ? 0 : 1;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      title: 'Number Trivia',
+      theme: ThemeData(
+        primaryColor: Colors.green.shade800,
+        accentColor: Colors.green.shade600,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Text(
-                'This Is A Counter',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: Text(
-                '$_counter',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: _colorList[numType(_counter)],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          incrementValue();
-        },
-        child: Icon(Icons.add),
-      ),
+      home: NumberTriviaPage(),
     );
   }
 }
